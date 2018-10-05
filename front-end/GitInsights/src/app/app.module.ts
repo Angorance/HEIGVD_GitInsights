1ca4171464c1b1b2aef4094809c81764d02a7bae
Unknown <bryan.curchod@gmail.com>
2018-10-05 10:59:32 +0200

    Angular Material imported

--- front-end/GitInsights/src/app/app.module.ts
+++ front-end/GitInsights/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
