870a4c135ed230f7d1d72ba3c82b771de27865d1
Daniel Gonzalez Lopez <idaniel.gonzalezlopezi@gmail.com>
2018-10-05 11:02:21 +0200

    Add some missing files

---
+++ server/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        "arrow-body-style": "off",
+        "arrow-parens": "off",
+        "no-console": ["error", { "allow": ["warn", "error"] }],
+    },
+};
