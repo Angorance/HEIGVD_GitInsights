b3764aaaaa8a6d36c02a2138985e87d175e82329
Unknown <bryan.curchod@gmail.com>
2018-10-18 15:14:19 +0200

    Redirection to stat-page (shortcut from home)

--- front-end/GitInsights/src/app/app.module.ts
+++ front-end/GitInsights/src/app/app.module.ts
@@ -5,22 +5,37 @@ import { MatCardModule } from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 
-
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { BtnLoginComponent } from './btn-login/btn-login.component';
 import { HttpClientModule } from '@angular/common/http';
 
+import { RouterModule, Routes } from '@angular/router';
+import { StatPageComponent } from './stat-page/stat-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+
+const appRoutes : Routes = [
+  {path:"stats", component:StatPageComponent},
+  {path:"home", component:LoginPageComponent},
+  {path:"", redirectTo:"/home", pathMatch:"full"},
+  {path:"**", component:PageNotFoundComponent}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
-    BtnLoginComponent
+    BtnLoginComponent,
+    StatPageComponent,
+    PageNotFoundComponent,
+    LoginPageComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatCardModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true}),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front-end/GitInsights/src/app/stat-page/stat-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stat-page',
+  templateUrl: './stat-page.component.html',
+  styleUrls: ['./stat-page.component.css']
+})
+export class StatPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
