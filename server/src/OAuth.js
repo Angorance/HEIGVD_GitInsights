0e88e2784acfc12bc0f79561e2bb07432657dd62
Helena <helena.line.reymond@gmail.com>
2018-10-22 12:04:20 +0200

    Access_token received #2

--- server/src/OAuth.js
+++ server/src/OAuth.js
@@ -5,29 +5,10 @@ const fetch = require('node-fetch'); // Used to get data from URL
 const clientId = '2a9a479e2953860bbd89';
 const clientSecret = '7100d1e611dd28e989fd81009f4e78a09e96ecaa';
 
-/*function findAName(code) {
-  return {
-    "client_id": clientId,
-    "client_secret": clientSecret,
-    "code": code,
-  };
-} */
-
 function oauthCallback(req, res, next) {
   const codeReceived = req.query.code;
 
   console.log(`Code received : ${codeReceived}`);
-
-  /*const reqToken = {
-    uri: 'https://github.com/login/oauth/access_token',
-    body: JSON.stringify(findAName(code)),
-    method: 'POST',
-  };
-
-  request(reqToken, (error, response) => {
-    console.log(`Access Token received : ${response.access_token}`);
-    console.log(`Error : ${error}`);
-  }); */
 
   const url = 'https://github.com/login/oauth/access_token';
   const body = {
@@ -47,7 +28,7 @@ function oauthCallback(req, res, next) {
 
   fetch(url, options)
     .then(data => data.json())
-    .then(res => console.log(res))
+    .then(res => console.log(`Access-token received : ${res.access_token}`))
     .catch(error => console.log(error));
 }
 
