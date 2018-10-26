d6b5105d241bd73529c5bab14eb388785e39a8ae
Unknown <bryan.curchod@gmail.com>
2018-10-26 19:14:07 +0200

    LES FLAGS :DDDDD

--- front-end/GitInsights/src/app/stat-page/stat-page.component.ts
+++ front-end/GitInsights/src/app/stat-page/stat-page.component.ts
@@ -1,35 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Chart } from 'chart.js';
+import { Country } from 'country-list';
 
 @Component({
   selector: 'app-stat-page',
   templateUrl: './stat-page.component.html',
   styleUrls: ['./stat-page.component.css']
 })
+
 export class StatPageComponent implements OnInit {
 
   tips = ['a random tip',
     'another tip',
     'oh my, so much tip just for me',
-    'this one isn\'t very relevant (it was Nybar\'s idea ;) )',
+    'this one isn\'t very relevant ',
     'this is the last one, promised',
     'ahah just kidding, this is the last :)'
   ];
 
   chart = [];
 
+  countries = require('country-list')();
+  flagClasses = [
+    'flag-icon-background',
+    'flag-icon-squared'
+  ];
+
   constructor(private http: HttpClient) {
   }
 
   ngOnInit() {
-    this.getData();
+    console.log();
+    this.flagClasses.push('flag-icon-' + this.countries.getCode('Iceland').toLowerCase());
   }
 
   getData(): void {
-    const urlServer: string = 'https://tweb-project1-serveur.herokuapp.com/user';
+    const urlServer = 'https://tweb-project1-serveur.herokuapp.com/user';
 
-    let getUrl = urlServer + '?access_token=' + sessionStorage.getItem('token');
+    const getUrl = urlServer + '?access_token=' + sessionStorage.getItem('token');
 
     console.log(getUrl);
 
