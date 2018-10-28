df941e86fe103fd5e8b5f229976b516e1e009fbf
Unknown <bryan.curchod@gmail.com>
2018-10-28 22:36:22 +0100

    Stat-page typscript comments

--- front-end/GitInsights/src/app/stat-page/stat-page.component.ts
+++ front-end/GitInsights/src/app/stat-page/stat-page.component.ts
@@ -1,46 +1,44 @@
 import { ErrorDialogComponent } from './../error-dialog/error-dialog.component';
 import {
   Component, OnInit, ViewChild, ElementRef,
-  Inject, HostBinding } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Chart } from "chart.js";
-import * as Country from "country-list";
+  Inject, HostBinding } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Chart } from 'chart.js';
+import * as Country from 'country-list';
 import { trigger, state, style,
-  animate, transition } from "@angular/animations";
+  animate, transition } from '@angular/animations';
 import { MatDialog, MatDialogRef } from '@angular/material';
 
 @Component({
-  selector: "app-stat-page",
-  templateUrl: "./stat-page.component.html",
-  styleUrls: ["./stat-page.component.css"],
-  animations: [
-    trigger("endLoad", [
+  selector: 'app-stat-page',
+  templateUrl: './stat-page.component.html',
+  styleUrls: ['./stat-page.component.css'],
+  animations: [ // animation for the loading screen
+    trigger('endLoad', [
       state(
-        "opened",
+        'opened',
         style({
-          height: "100%",
+          height: '100%',
           opacity: 1
         })
       ),
       state(
-        "closed",
+        'closed',
         style({
-          height: "0%",
-          opacity: 0
+          height: '0%',
+          opacity: 0 // we set the opacity to 0, otherwise the spinner would still be visible
         })
       ),
-      transition("opened => closed", [animate("0.5s ease-in-out")])
+      transition('opened => closed', [animate('0.5s ease-in-out')])
     ])
   ]
 })
 
 export class StatPageComponent implements OnInit {
-  @ViewChild("canvas")
+  @ViewChild('canvas')
   canvas: ElementRef;
   context: CanvasRenderingContext2D;
 
-  // test purpose ==>
-  results = '{"country":"Switzerland","profile_picture":"https://avatars1.githubusercontent.com/u/30982987?v=4","favLanguages":{"Java":1203794,"Dockerfile":1050,"CSS":267635,"C++":797671,"C":33529,"CMake":1228,"JavaScript":538538,"PHP":83311,"Shell":8185,"HTML":3942,"TypeScript":31503,"PLpgSQL":1946,"QMake":7571,"Makefile":79446},"repositories":[{"label":"Created","value":23},{"label":"Forked","value":10},{"label":"Stars","value":3}],"issues":[{"label":"Opened","value":0},{"label":"Closed","value":31}],"milestones":[{"date":"2017-08-13T16:51:57Z","label":"account creation"},{"date":"2017-09-23T15:39:32Z","label":"first repository"},{"date":"2017-09-23T15:38:49Z","label":"first commit"}],"trivia":[{"label":"Coded lines","value":905019},{"label":"Commits","value":200}],"tips":[]}';
   tips: Array<Tip> = [];
   chart = [];
   issues: Array<{label: string, value: number}> = [];
@@ -55,7 +53,7 @@ export class StatPageComponent implements OnInit {
     '#C90000'
   ];
 
-  // define if the application has gathered all the data needed
+  // define if the application has gathered all the data needed (loading screen)
   loaded = false;
 
   // style for the avatar div, to display the picture in background
@@ -63,41 +61,49 @@ export class StatPageComponent implements OnInit {
 
   // object for the country-list package (npm)
   countries = Country();
+
   // classes for the flag icon
-  flagClasses = ["flag-icon-background", "flag-icon-squared"];
+  flagClasses = ['flag-icon-background', 'flag-icon-squared'];
 
-  _srvAddress = 'https://tweb-project1-serveur.herokuapp.com'; // "http://localhost:3000";
+  // server URL used
+  _srvAddress = 'https://tweb-project1-serveur.herokuapp.com';
 
+  // dialog box in case of error
   errorDialogRef: MatDialogRef<ErrorDialogComponent>;
 
   constructor(private http: HttpClient, public dialog: MatDialog) {}
   ngOnInit() {
-    /*this.setData(JSON.parse(this.results));
-    this.loaded = true;*/
-    //this.getData();
+
+    // we have to retrieve the token to make request to the server
     this.getToken();
   }
 
+  /**
+   * Use the code given by GitHub OAuth service to ask the server to retrieve 
+   * the user token. If a token is already available, we simply bypass this step
+   * When we have a token we ask the server to gather the user informations
+   */
   getToken() : void {
+
+    // check token presence
     if (sessionStorage.getItem('token') === null) {
       // no token found, we have to get it
+
       const srvTokenService = '/authenticate';
-
-      // we came from github so we have to get the code returned
       const callbackCode: URLSearchParams = new URLSearchParams(window.location.href.split('?')[1]);
-
-      // url to send the request to
       const getUrl = this._srvAddress + srvTokenService + '?code=' + callbackCode.get('code');
 
       // send the request to the server
       this.http.get(getUrl).toPromise()
       .then(
         res => {
+          // token successfully retreived, we store it for the session
           sessionStorage.setItem('token', res['access_token']);
           this.getData();
         }
       )
       .catch(err => {
+        // mistakes were mades :(
         this.openDialogue();
       });
     } else {
@@ -106,19 +112,51 @@ export class StatPageComponent implements OnInit {
     }
   }
 
+  /**
+   * Ask the server to retreive the user data and use them.
+   * One the datas are retrieved and the UI is ready, trigger the
+   * loading screen removal
+   */
+  getData(): void {
+    // we have to give the user token to the server
+    const srvService = '/user';
+    const getUrl = this._srvAddress + srvService + '?access_token=' + sessionStorage.getItem('token');
+
+    // make the request and wait for the response
+    this.http
+      .get(getUrl)
+      .toPromise()
+      .then((res: GitData) => {
+        // we have our datas
+        this.setData(res);
+        this.loaded = true;
+      })
+      .catch(err => {
+
+        // mistakes were made :(
+        this.openDialogue();
+      });
+  }
+
+  /**
+   * Use and display the differents user data provided by the server and the GitHun API
+   * @param res Object representing the user data to use for the stats and the tips
+   */
   setData(res: GitData) {
 
-    // set the flag and the avatar
+    // set the avatar and the flag
     this.avatarStyle = {
-      "background-image": 'url("' + res.profile_picture + '")'
+      'background-image': 'url("' + res.profile_picture + '")'
     };
+
     if (res.country) {
+      // not everyone has set their country...
       this.flagClasses.push(
-        "flag-icon-" + this.countries.getCode(res.country).toLowerCase()
+        'flag-icon-' + this.countries.getCode(res.country).toLowerCase()
       );
     }
 
-    // set the issues, trivia, and repositories (list of object { label, value })
+    // set the issues, trivia, repositories and tips
     this.issues = res.issues;
     this.trivias = res.trivia;
     this.repositories = res.repositories;
@@ -129,7 +167,7 @@ export class StatPageComponent implements OnInit {
       this.milestones = res.milestones;
     }
 
-    // chart creation - creating a list containing a the language and the total line number
+    // chart creation - we have to create an array containing all the language and the total line number
     if (res.favLanguages) {
 
       let labels: string[] = Object.keys(res.favLanguages); // languages
@@ -139,37 +177,35 @@ export class StatPageComponent implements OnInit {
         this.languages.push({ label: labels[_i], value: datas[_i] });
       }
 
-      // reset the list to store the top 5
+      // reset the list to store the top languages used
       labels = [];
       datas = [];
       this.languages
         .sort(compareLanguage)
-        .slice(0, 7)
+        .slice(0, 7) // we only keep the top 7
         .forEach(elem => {
           labels.push(elem.label);
           datas.push(elem.value);
         });
 
       // chart creation
-      this.context = (<HTMLCanvasElement>this.canvas.nativeElement).getContext(
-        "2d"
-      );
+      this.context = (<HTMLCanvasElement>this.canvas.nativeElement).getContext('2d');
       this.chart = new Chart(this.context, {
-        type: "horizontalBar",
+        type: 'horizontalBar',
         data: {
           labels: labels,
           datasets: [
             {
               data: datas,
               backgroundColor: [
-                "rgb(72, 66, 244)",
-                "rgba(54, 162, 235, 1)",
-                "rgb(36, 122, 118)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-                "rgb(72, 66, 244)",
-                "rgba(54, 162, 235, 1)",
-                "rgb(36, 122, 118)",
+                'rgb(72, 66, 244)',
+                'rgba(54, 162, 235, 1)',
+                'rgb(36, 122, 118)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgb(72, 66, 244)',
+                'rgba(54, 162, 235, 1)',
+                'rgb(36, 122, 118)',
               ]
             }
           ]
@@ -183,7 +219,7 @@ export class StatPageComponent implements OnInit {
               {
                 scaleLabel: {
                   display: true,
-                  labelString: "Coded lines"
+                  labelString: 'Coded lines'
                 }
               }
             ]
@@ -194,32 +230,21 @@ export class StatPageComponent implements OnInit {
     }
   }
 
-  getData(): void {
-    const srvService = "/user"; // 'https://tweb-project1-serveur.herokuapp.com/user';
-
-    const getUrl = this._srvAddress +
-    srvService + "?access_token=" + sessionStorage.getItem("token");
-
-    this.http
-      .get(getUrl)
-      .toPromise()
-      .then((res: GitData) => {
-
-        this.setData(res);
-        this.loaded = true;
-      })
-      .catch(err => {
-        this.openDialogue();
-      });
-  }
-
-  getTipStyle(i: number) {
-
+  /**
+   * Set a specific font color considering the "score" of the user
+   * 
+   * @param quality position in the quality tab to use
+   */
+  getTipStyle(quality: number) {
     return {
-      'color' : this.qualityTab[i % this.qualityTab.length],
+      'color' : this.qualityTab[quality % this.qualityTab.length],
     };
   }
 
+  /**
+   * Open a dialog box. When the dialog is closed, we clear the token storage, and 
+   * redirect the user to the home page
+   */
   openDialogue(): void {
     this.errorDialogRef = this.dialog.open(ErrorDialogComponent, {
       width: '60%',
@@ -233,7 +258,12 @@ export class StatPageComponent implements OnInit {
   }
 }
 
-function compareLanguage(a, b) {
+/**
+ * Compare two languages
+ * @param a first language to compare
+ * @param b second language to compare
+ */
+function compareLanguage(a: {label:string, value:number}, b: {label:string, value:number}) {
   if (a.value < b.value) {
     return 1;
   }
@@ -245,6 +275,9 @@ function compareLanguage(a, b) {
   return 0;
 }
 
+/**
+ * class representing the datas gathered by the server
+ */
 class GitData {
   country: string;
   profile_picture: string;
@@ -256,10 +289,13 @@ class GitData {
   tips: Array<Tip>;
 }
 
+/**
+ * represent a tip
+ */
 class Tip {
-  title: string;
-  score:  number;
-  criteria: string;
-  quality: number;
+  title: string; // category for the tip
+  score:  number; 
+  criteria: string; // how did we judge the user
+  quality: number; // how well did he do
   tip: string;
 }
